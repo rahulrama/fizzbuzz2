3f6be6583110b42c32a347107bfb9bbbccd02341
rahulrama <rahulrama@gmail.com>
2016-04-07 12:02:20 +0100

    More test cases added : round 2

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-   it 'returns "fizz" when passed 3' do
-     expect(fizzbuzz(3)).to eq 'fizz'
+   it 'returns "fizzbuzz" when passed multiples of 3 and 5' do
+     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+   end
+     it 'returns "fizz" when passed multiples of 3' do
+       expect(fizzbuzz(9)).to eq 'fizz'
+     end
+       it 'returns "buzz" when passed multiples of 5' do
+         expect(fizzbuzz(10)).to eq 'buzz'
+       end
+         it 'returns 1 when passed 1' do
+           expect(fizzbuzz(1)).to eq 1
+         end
 end
-end
-

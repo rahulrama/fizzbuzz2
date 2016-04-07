800a17d0a237d60c38afcff447928cbeebd43917
Amy Nicholson <missamynicholson@gmail.com>
2016-04-07 12:27:32 +0100

    Update

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
-def fizzbuzz(number)
-	return "fizzbuzz" if number%3==0 && number%5==0
-	return "fizz" if number%3==0
-	return "buzz" if number%5==0
-	number
+class Integer
+def fizzbuzz
+	return "fizzbuzz" if self%3==0 && self%5==0
+	return "fizz" if self%3==0
+	return "buzz" if self%5==0
+	self
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,17 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
    it 'returns "fizzbuzz" when passed multiples of 3 and 5' do
-     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+     expect(15.fizzbuzz).to eq 'fizzbuzz'
    end
      it 'returns "fizz" when passed multiples of 3' do
-       expect(fizzbuzz(9)).to eq 'fizz'
+       expect(9.fizzbuzz).to eq 'fizz'
      end
        it 'returns "buzz" when passed multiples of 5' do
-         expect(fizzbuzz(10)).to eq 'buzz'
+         expect(10.fizzbuzz).to eq 'buzz'
        end
          it 'returns 1 when passed 1' do
-           expect(fizzbuzz(1)).to eq 1
+           expect(1.fizzbuzz).to eq 1
          end
+
+
 end
